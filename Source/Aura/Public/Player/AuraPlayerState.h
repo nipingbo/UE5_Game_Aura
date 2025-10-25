62073f1023c388a53f19834aa18e089afef8d818
nipingbo <nipingbo@gmail.com>
2025-10-25 14:34:14 +0800

    setup own player state

---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Raymond
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+public:
+	AAuraPlayerState();
+	GENERATED_BODY()
+	
+};
