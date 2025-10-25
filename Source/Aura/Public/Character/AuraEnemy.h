e725a243f6d129dfb82e06e06349cda3d438ecdf
nipingbo <nipingbo@gmail.com>
2025-10-25 17:18:21 +0800

    Setup Ability Actor Info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -15,9 +15,14 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 
-public:
+	public:
 	AAuraEnemy();
+
+	/*Enemy Interface*/
 	virtual void HighLightActor() override;
 	virtual void UnHighLightActor() override;
+	/*End of Enemy Interface*/
+	protected:
+	virtual void BeginPlay() override;
 	
 };
