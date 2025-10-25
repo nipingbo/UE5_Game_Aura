e28aa55097ed54330e5183a37bed6bfbc06f5d5e
nipingbo <nipingbo@gmail.com>
2025-10-25 10:45:13 +0800

    Setup GameMode

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Raymond
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Raymond
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
