628a8da83ca08fcd8e51af97327a92d16971488a
Raymond Ni <nipingbo@gmail.com>
2025-11-05 20:22:55 +0800

    Refactor ability actor info initialization logic

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -9,6 +9,7 @@
 #include "Player/AuraPlayerController.h"
 #include "Player/AuraPlayerState.h"
 #include "UI/HUD/AuraHUD.h"
+#include "Blueprint/UserWidget.h"
 
 AAuraCharacter::AAuraCharacter()
 {
@@ -25,15 +26,15 @@ AAuraCharacter::AAuraCharacter()
 void AAuraCharacter::PossessedBy(AController* NewController)
 {
 	Super::PossessedBy(NewController);
-	//init ability actor info for the server
+	// init ability actor info for the server
 	InitAbilityActorInfo();
 }
 
 void AAuraCharacter::OnRep_Controller()
 {
 	Super::OnRep_Controller();
-	//init ability actor info for the client
-	//InitAbilityActorInfo();
+	// init ability actor info for the client when controller is replicated
+	InitAbilityActorInfo();
 }
 
 void AAuraCharacter::OnRep_PlayerState()
@@ -48,19 +49,42 @@ void AAuraCharacter::InitAbilityActorInfo()
 	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
 	if (!AuraPlayerState) return;
 	
-	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
-	//inform the abilitySystem is set
-	Cast<UAuraAbilitySystemComponent>(AuraPlayerState->GetAbilitySystemComponent())->AbilityActorInfoSet();
-	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
+	UAbilitySystemComponent* ASC = AuraPlayerState->GetAbilitySystemComponent();
+	if (!ASC) return;
+
+	// 如果 ASC 已经对同一个 Owner/Avatar 初始化过，则跳过重复初始化
+	if (ASC->AbilityActorInfo
+		&& ASC->AbilityActorInfo->Owner == AuraPlayerState
+		&& ASC->AbilityActorInfo->AvatarActor == this)
+	{
+		// 仍然缓存指针以备后续使用
+		AbilitySystemComponent = ASC;
+		AttributeSet = AuraPlayerState->GetAttributeSet();
+		return;
+	}
+
+	// (重新)初始化 actor info：Owner 是 PlayerState，Avatar 是 Character
+	ASC->InitAbilityActorInfo(AuraPlayerState, this);
+
+	// 仅在成功 cast 为项目自定义的 AuraASC 时调用子类扩展方法
+	if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(ASC))
+	{
+		AuraASC->AbilityActorInfoSet();
+	}
+
+	AbilitySystemComponent = ASC;
 	AttributeSet = AuraPlayerState->GetAttributeSet();
 
-	//check means if the object is null then the engine will crash
-	//but in this case, we check whether it is null or not , we don't want the engine crush
+	// 初始化 HUD/Overlay — HUD 自身也应防止重复创建 widget
 	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
 	{
 		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(AuraPlayerController->GetHUD()))
 		{
-			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
+			// HUD 会判断自己是否已初始化 Overlay
+			if (!AuraHUD->IsOverlayInitialized())
+			{
+				AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
+			}
 		}
 	}
 }
