d4689a68a9509cdb865c3d39bc5d2e70b6979e44
nipingbo <nipingbo@gmail.com>
2025-11-22 21:23:29 +0800

    Create a singleton gameplay tags class

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,13 @@
+// Copyright Raymond
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduce damage taken, improves Block Chance"));
+	
+}
