a3710c9aa92cd4db73ec695c093d1d712a77b4ca
nipingbo <nipingbo@gmail.com>
2025-11-27 20:09:15 +0800

    Response attributes changes

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -26,5 +26,7 @@ public:
 protected:
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
-	
+
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;	
 };
