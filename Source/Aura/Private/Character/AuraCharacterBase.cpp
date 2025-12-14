7e2dc89ec61563b316f26f57cfe492771b8ffedc
nipingbo <nipingbo@gmail.com>
2025-12-14 17:42:38 +0800

    Enemy Dissolve Effect

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -52,6 +52,7 @@ void AAuraCharacterBase::MulticastHandleDeath_Implementation()
 	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Dissolve();
 }
 
 void AAuraCharacterBase::BeginPlay()
@@ -105,5 +106,21 @@ void AAuraCharacterBase::AddCharacterAbilities()
 		 AuraASC->AddCharacterAbilities(StartupAbilities);
 	 }
 	
+}
+
+void AAuraCharacterBase::Dissolve()
+{
+	if (IsValid(DissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+		StartDissolveTimeline(DynamicMatInst);
+	}
+	if (IsValid(WeaponDissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* WeaponDynamicMatInst = UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+		Weapon->SetMaterial(0, WeaponDynamicMatInst);
+		StartWeaponDissolveTimeline(WeaponDynamicMatInst);
+	}
 }
 
