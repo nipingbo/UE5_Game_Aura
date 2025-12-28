cf9be68e34fdded759a84df99f4c5ab35beedf84
nipingbo <nipingbo@gmail.com>
2025-12-28 16:27:27 +0800

    Get live player within Radius

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -35,4 +35,11 @@ public:
 	UAnimMontage* GetHitReactMontage();
 
 	virtual void Die() = 0;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool IsDead() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	AActor* GetAvatar();
+	
 };
