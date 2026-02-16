98f3880d516b8de4223f2a07ac65c1b04a5c923a
nipingbo <nipingbo@gmail.com>
2026-02-16 23:23:03 +0800

    Construct SpellMenuWidgetController

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -3,3 +3,12 @@
 
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -11,6 +11,7 @@ struct FGameplayEffectContextHandle;
 class UAbilitySystemComponent;
 class UAttributeMenuWidgetController;
 class UOverlayWidgetController;
+class USpellMenuWidgetController;
 /**
  * 
  */
@@ -19,11 +20,17 @@ class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 public:
-	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController")
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController", meta = (DefaultToSelf = "WorldContextObject"))
+	static bool MakeWidgetControllerParams(const UObject* WorldContextObject, FWidgetControllerParams& OutWCParams, AAuraHUD*& OutAuraHUD);
+	
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController", meta = (DefaultToSelf = "WorldContextObject"))
 	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
 
-	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController")
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController", meta = (DefaultToSelf = "WorldContextObject"))
 	static UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const UObject* WorldContextObject);
+	
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController", meta = (DefaultToSelf = "WorldContextObject"))
+	static USpellMenuWidgetController* GetSpellMenuWidgetController(const UObject* WorldContextObject);
 
 	UFUNCTION(BlueprintCallable, Category="AuraAbilitySystemLibrary|CharacterClassDefaults")
 	static void InitializeDefaultAttributes(const UObject* WorldContextObject, ECharacterClass CharacterClass, float Level, UAbilitySystemComponent* ASC);
