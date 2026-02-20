0dcad30c76f5a698dd3c61cadda41a92a7cb2e45
nipingbo <nipingbo@gmail.com>
2026-02-20 20:04:20 +0800

    Spell Descriptions

--- Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -8,7 +8,7 @@
 #include "GameplayTagContainer.h"
 #include "SpellMenuWidgetController.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FSpellGlobeSelectedSignature, bool, bSpendPointsButtonEnabled, bool, bEquippedButtonEnabled);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FSpellGlobeSelectedSignature, bool, bSpendPointsButtonEnabled, bool, bEquippedButtonEnabled, FString, DescriptionString, FString, NextLevelDescriptionString);
 
 struct FSelectedAbility
 {
