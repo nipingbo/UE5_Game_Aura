bf70d75afd478cc75a18f2e3fbba8774b5ab3930
nipingbo <nipingbo@gmail.com>
2026-02-22 14:21:50 +0800

    Debuff Parameters

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -57,6 +57,11 @@ public:
 	FGameplayTag Debuff_Stun;
 	FGameplayTag Debuff_Arcane;
 	FGameplayTag Debuff_Physical;
+	
+	FGameplayTag Debuff_Chance;
+	FGameplayTag Debuff_Damage;
+	FGameplayTag Debuff_Duration;
+	FGameplayTag Debuff_Frequency;
 
 	FGameplayTag Abilities_None;
 	FGameplayTag Abilities_Attack;
