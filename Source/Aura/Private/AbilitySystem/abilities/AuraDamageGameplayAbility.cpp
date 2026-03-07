3c29130d9229868b0c5dd9eff63d1b3ed8861ae3
nipingbo <nipingbo@gmail.com>
2026-03-07 21:50:18 +0800

    Death Impulse Magnitude

--- Source/Aura/Private/AbilitySystem/abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/abilities/AuraDamageGameplayAbility.cpp
@@ -33,6 +33,7 @@ FDamageEffectParams UAuraDamageGameplayAbility::MakeDamageEffectParamsFromClassD
 	Params.DebuffDamage = DebuffDamage;
 	Params.DebuffDuration = DebuffDuration;
 	Params.DebuffFrequency = DebuffFrequency;
+	Params.DeathImpulseMagnitude = DeathImpulseMagnitude;
 	return Params;
 	
 }
