a6b9fe1c68c045c5ac7536a758a5800cb84c3a10
nipingbo <nipingbo@gmail.com>
2026-03-30 10:53:06 +0800

    FireBall Timeline

--- Source/Aura/Private/AbilitySystem/abilities/AuraFireBlast.cpp
+++ Source/Aura/Private/AbilitySystem/abilities/AuraFireBlast.cpp
@@ -92,6 +92,7 @@ TArray<AAuraFireBall*> UAuraFireBlast::SpawnFireBalls()
 			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
 		
 		FireBall->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
+		FireBall->ReturnToActor = GetAvatarActorFromActorInfo();
 		FireBalls.Add(FireBall);
 		FireBall->FinishSpawning(SpawnTransform);
 	}
