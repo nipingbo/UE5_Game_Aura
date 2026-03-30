4020ec0eb2f8be82797c19898f0c654386daeeb8
nipingbo <nipingbo@gmail.com>
2026-03-30 19:48:00 +0800

    FireBall Explosion Damage

--- Source/Aura/Private/AbilitySystem/abilities/AuraFireBlast.cpp
+++ Source/Aura/Private/AbilitySystem/abilities/AuraFireBlast.cpp
@@ -93,6 +93,10 @@ TArray<AAuraFireBall*> UAuraFireBlast::SpawnFireBalls()
 		
 		FireBall->DamageEffectParams = MakeDamageEffectParamsFromClassDefaults();
 		FireBall->ReturnToActor = GetAvatarActorFromActorInfo();
+		
+		FireBall->ExplosionDamageParams = MakeDamageEffectParamsFromClassDefaults();
+		
+		FireBall->SetOwner(GetAvatarActorFromActorInfo());
 		FireBalls.Add(FireBall);
 		FireBall->FinishSpawning(SpawnTransform);
 	}
